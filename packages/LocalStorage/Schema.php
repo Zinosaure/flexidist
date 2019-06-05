c54fce3d013efee53d399bbc7e152891b986c80d
Zinosaure <ashleydambra@live.com>
2019-06-05 20:37:15 +0200

    LocalStorage\Schema VALIDATE_SCHEMA can extends class of Schema instance

--- packages/LocalStorage/Schema.php
+++ packages/LocalStorage/Schema.php
@@ -15,10 +15,10 @@ abstract class Schema {
     */
     const SCHEMA_VALIDATE_STRING            = 'is_string';
     const SCHEMA_VALIDATE_NUMERIC           = 'is_numeric';
-    const SCHEMA_VALIDATE_INT               = 'is_int';
-    const SCHEMA_VALIDATE_INTEGER           = 'is_int';
-    const SCHEMA_VALIDATE_BOOLEAN           = 'is_bool';
-    const SCHEMA_VALIDATE_LIST              = 'is_array';
+    const SCHEMA_VALIDATE_INT				 = 'is_int';
+    const SCHEMA_VALIDATE_INTEGER        = 'is_int';
+    const SCHEMA_VALIDATE_BOOLEAN	= 'is_bool';
+    const SCHEMA_VALIDATE_LIST			= 'is_array';
 
     const VALIDATE_SCHEMA                   = [];
 
@@ -37,15 +37,17 @@ abstract class Schema {
         $data_validated = [];
 
         foreach (array_replace($validate_schema = $validate_schema ?: static::VALIDATE_SCHEMA, $data) as $field => $value) {
-            if (isset($validate_schema[$field]) 
+            if (isset($validate_schema[$field])
                 && ($sField = $validate_schema[$field])
-                && (is_array($sField) || is_callable($sField))
+                && (is_array($sField) || is_callable($sField) || class_exists($sField))
             ) {
                 if (is_array($sField))
                     $data_validated[$field] = $this->bind($data[$field] ?? [], $sField);
+                else if (isset($data[$field]) && is_callable($method = $sField . '::getSchemaValidateType') && ($object = $method ($data[$field])) instanceOf self)
+                    $data_validated[$field] = $object;
                 else if (isset($data[$field]))
                     $data_validated[$field] = $sField($data[$field]) ? $data[$field] : null;
-                else 
+                else
                     $data_validated[$field] = $sField == 'is_array' ? [] : null;
             } else if (isset($validate_schema[$field]))
                 $data_validated[$field] = null;
@@ -57,8 +59,8 @@ abstract class Schema {
     /**
     *
     */
-    final public static function getSchemaValidateType() {
-        return 'd';
+    final public static function getSchemaValidateType($data) {
+    	return new static($data);
     }
 }
 ?>
