b77aac3fd80fbf0e9e3078fd61cb7d542fd7ca60
Gino D <gdambra@facepoint.co>
2019-06-10 13:44:18 +0200

    Schema\JSON implements ListOf

--- packages/Flexidist/Server/Schema/JSON.php
+++ packages/Flexidist/Server/Schema/JSON.php
@@ -8,6 +8,12 @@ namespace Flexidist\Server\Schema;
 /**
 *
 */
+
+interface ListOf {}
+
+/**
+*
+*/
 abstract class JSON {
 
     /**
@@ -19,6 +25,7 @@ abstract class JSON {
     const SCHEMA_VALIDATE_INTEGER = 'is_int';
     const SCHEMA_VALIDATE_BOOLEAN = 'is_bool';
     const SCHEMA_VALIDATE_LIST = 'is_array';
+    const SCHEMA_VALIDATE_LIST_OF = 'a_list_of:';
 
     const VALIDATE_SCHEMA = [];
     const SCHEMA_PRIMARY_KEY = null;
@@ -34,9 +41,11 @@ abstract class JSON {
             if (!isset($validate_schema[$field]))
                 continue;
 
-            if (class_exists($sField = $validate_schema[$field]))
-                $this->{$field} = ($object = new $sField((array) ($data[$field] ?? []))) instanceOf self ? $object : null;
-            else if ($sField == self::SCHEMA_VALIDATE_LIST)
+            if (class_exists($sField = $validate_schema[$field]) && $object = new $sField((array) ($data[$field] ?? [])))
+                $this->{$field} = $object instanceOf self ? $object : null;
+            else if (strpos($sField, self::SCHEMA_VALIDATE_LIST_OF) !== false && isset($data[$field])) {
+                $this->{$field} = is_array($data[$field]) ? $data[$field] : [];
+            } else if ($sField == self::SCHEMA_VALIDATE_LIST)
                 $this->{$field} = array_values($data[$field] ?? []);
             else
                 $this->{$field} = $data[$field] ?? null; 
@@ -57,21 +66,58 @@ abstract class JSON {
         if (!isset(static::VALIDATE_SCHEMA[$name]))
             return;
         
-        $callback = static::VALIDATE_SCHEMA[$name];
+        $sField = static::VALIDATE_SCHEMA[$name];
         $attribute = $this->vars[$name] ?? null;
         
-        if (is_null($attribute) 
+        if (strpos($sField, self::SCHEMA_VALIDATE_LIST_OF) !== false && ($instance_of = str_replace(self::SCHEMA_VALIDATE_LIST_OF, null, $sField))) {
+            $this->vars[$name] = new class($instance_of, $mixed_value) implements ListOf {
+            
+                protected $instance_of = null;
+                protected $data = [];
+    
+                public function __construct(string $instance_of, $data) {
+                    $this->instance_of = $instance_of;
+    
+                    foreach ((array) $data as $i => $mixed_value)
+                        $this->set($i, $mixed_value);
+                }
+    
+                public function items(): array {
+                    return $this->data;
+                }
+    
+                public function item($index) {
+                    return $this->data[$index] ?? null;
+                }
+    
+                public function set($index, $mixed_value) {
+                    if ((is_array($mixed_value) && ($object = new $this->instance_of($mixed_value))) || ($object = $mixed_value) instanceOf $this->instance_of) {
+                        if (!is_null($index))
+                            $this->data[$index] = $object;
+                        else
+                            $this->data[] = $object;
+                    }
+                }
+    
+                public function unset($index) {
+                    if (is_object($index))
+                        $index = array_search($index, $this->data);
+    
+                    unset($this->data[$index]);
+                }
+            };
+        } else if (is_null($attribute) 
             && !is_null($mixed_value) 
             && (
-                (is_callable($callback) && $callback($mixed_value))
-                || $mixed_value instanceOf $callback
+                (is_callable($sField) && $sField($mixed_value))
+                || $mixed_value instanceOf $sField
             )
         )
             $this->vars[$name] = $mixed_value;
         else if (!is_null($attribute) 
             && !is_null($mixed_value) 
             && (
-                (is_callable($callback) && $callback($mixed_value))
+                (is_callable($sField) && $sField($mixed_value))
                 || (is_object($attribute) && is_a($mixed_value, get_class($attribute)))
             )
         )
@@ -90,34 +136,11 @@ abstract class JSON {
     /**
     *
     */
-    final public static function create(array $data = [], array $validate_schema = []): self {
-        return new static($data, $validate_schema);
-    }
-
-    /**
-    *
-    */
     final public function ID() {
         if (static::SCHEMA_PRIMARY_KEY)
             return $this->{static::SCHEMA_PRIMARY_KEY};
         
         return null;
-    }
-
-    /**
-    *
-    */
-    final public function exportIndexes($ID, string $filename): array {
-        $export_data = [];
-
-        foreach (static::SCHEMA_INDEX_KEYS as $i => $name) {
-            if ($this->{$name} instanceOf self)
-                $export_data[$name] = $this->{$name}->exportIndexes($ID, $filename);
-            else
-                $export_data[$name][$this->{$name}][$ID] = $filename;
-        }
-
-        return $export_data;
     }
     
     /**
